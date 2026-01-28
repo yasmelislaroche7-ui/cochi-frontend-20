ce107a82197d99466725fc4060c8023506eae3a0
yasmelislaroche7-ui <yasmelislaroche7@gmail.com>
2026-01-28 01:19:54 +0000

    Tx

---
+++ lib/contracts/addresses.ts
@@ -0,0 +1,7 @@
+// lib/contracts/addresses.ts
+
+export const STAKING_CONTRACT_ADDRESS =
+  process.env.NEXT_PUBLIC_STAKING_CONTRACT_ADDRESS as `0x${string}`
+
+export const TOKEN_CONTRACT_ADDRESS =
+  process.env.NEXT_PUBLIC_TOKEN_CONTRACT_ADDRESS as `0x${string}`
